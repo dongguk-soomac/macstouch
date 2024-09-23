a79abb0ee154a3821f18b50d7ef6fbce8f111ba4
choiyoonji <cyj21c6352@gmail.com>
2024-09-24 02:25:53 +0900

    Add: robot client code

--- src/macstouch_config.py
+++ src/macstouch_config.py
@@ -1,3 +1,3 @@
-MaterialList = ["bread", "meat", "cheeze", "pickle", "onion", "sauce", "tomato", "cabage"]
+MaterialList = ["bread", "meat", "cheeze", "pickle", "onion", "sauce", "tomato", "lettuce"]
 WeightLimit = [10, 10, 10, 10, 10, 10, 10, 10]
 ModeList = ["init_pos", "vision", "pnp", "tool_return"]
