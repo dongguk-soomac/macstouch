3982dacf6c09262a1633fd52f7aeaca6b97e7300
NO HYUNWOO <realsteel90@dgu.ac.kr>
2024-09-23 14:03:01 +0900

    Update init.py

--- src/zero/init.py
+++ src/zero/init.py
@@ -1,1 +1,20 @@
-# zero server
+# -*- coding: utf-8 -*-
+from rbsys import *
+
+#This is sample program for initial settings.
+
+if name == "main":
+  rbs = RobSys()
+  rbs.open()
+  a = rbs.cmd_reset()
+  print(a)
+  rbs.assign_din(run=0)
+
+  k=rbs.set_robtask("test_bottle.py")
+  rbs.assign_din(run=0, stop=1, err_reset=2, pause=3 )
+  rbs.assign_dout(running=16, svon=17, emo=18, in_pause=19)
+  print("test ", k)
+
+# rbs.cmd_run("test_bottle.py")
+  print(rbs.req_mcmd()[4])
+  rbs.close()
