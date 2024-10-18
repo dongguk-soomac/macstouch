17e2bd85aba1288164c768e748405382dd1568c6
JeongSeoJin <pysuk88@gmail.com>
2024-10-18 21:58:22 +0900

    Update: about custom menu list publishing

--- src/gui/menu_list_node.py
+++ src/gui/menu_list_node.py
@@ -2,6 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from macstouch.msg import order  # order.msg를 생성 후 컴파일하면 자동 생성
+from selected_menu import menu_index
 
 # 불고기 버거 = [2, 1, 0, 3, 8, 1, 1, 3]
 # 제로 버거 = [2, 0, 0, 3, 8, 1, 1, 3]
@@ -13,12 +14,7 @@ def menu_callback(data):
     global i
     rospy.loginfo(f"Received data: {data.data}")
 
-    # # 데이터가 숫자인지 확인
-    # if data.data.isdigit():
-    #     menu_index = int(data.data)
-    # else:
-    #     rospy.logwarn("Received invalid data. Not an integer.")
-    #     return
+    for_custom_list = [0, 0, 0, 0, 0, 0, 0, 0]
 
     menu_index = data.data
 
@@ -28,26 +24,41 @@ def menu_callback(data):
     order_msg = order()
     order_msg.id = i
 
-    # menu_index 값에 따라 menu 리스트 할당
-    if menu_index == "[0]":
-        order_msg.menu = [2, 1, 0, 3, 8, 1, 1, 3]  # 불고기 버거
-    elif menu_index == "[1]":
-        order_msg.menu = [2, 0, 0, 3, 8, 1, 1, 3]  # 제로 버거
-    elif menu_index == "[2]":
-        order_msg.menu = [2, 1, 1, 3, 8, 1, 1, 3]  # 치즈 버거
-    elif menu_index == "[3]":
-        order_msg.menu = [2, 1, 2, 3, 8, 1, 1, 3]  # 더블 치즈 버거
-    elif menu_index == "[4]":
-        order_msg.menu = [2, 1, 0, 3, 8, 1, 1, 3]  # 불고기 버거
-    elif menu_index == "[5]":
-        order_msg.menu = [2, 0, 0, 3, 8, 1, 1, 3]  # 제로 버거
-    elif menu_index == "[6]":
-        order_msg.menu = [2, 1, 1, 3, 8, 1, 1, 3]  # 치즈 버거
-    elif menu_index == "[7]":
-        order_msg.menu = [2, 1, 2, 3, 8, 1, 1, 3]  # 더블 치즈 버거
-    else:
-        rospy.logwarn("Invalid menu index received!")
-        return
+    for_len = []
+
+
+    ############ menu index에서 데이터만 뽑아 custom_list에 재배열
+
+    for j in range(len(menu_index)):
+        if menu_index[j].isdigit():  # 숫자인 경우만 변환
+            for_custom_list[int(menu_index[j])] += 1
+        else:
+            print(f"Invalid value: {menu_index[j]}")
+    ############################
+
+    # for_len = for_custom_list
+
+    # if 0 in for_len:
+    #     for_len.remove(0)
+    # else:
+    #     print("0 is not in the list")
+    print(len(menu_index))
+
+    if (len(menu_index)) > 3:
+        order_msg.menu = for_custom_list[:]
+
+    elif(len(menu_index)) == 3:
+        if menu_index == "[0]":
+            order_msg.menu = [2, 1, 0, 3, 8, 1, 1, 3]  # 불고기 버거
+        elif menu_index == "[1]":
+            order_msg.menu = [2, 0, 0, 3, 8, 1, 1, 3]  # 제로 버거
+        elif menu_index == "[2]":
+            order_msg.menu = [2, 1, 1, 3, 8, 1, 1, 3]  # 치즈 버거
+        elif menu_index == "[3]":
+            order_msg.menu = [2, 1, 2, 3, 8, 1, 1, 3]  # 더블 치즈 버거
+        else:
+            rospy.logwarn("Invalid menu index received!")
+            return
 
     # 퍼블리시
     order_pub.publish(order_msg)
@@ -63,7 +74,7 @@ def listener_and_publisher():
     # 서브스크라이버 생성: /menu_index 토픽을 구독
     rospy.Subscriber('/menu_index', String, menu_callback)
 
-    rospy.loginfo("Listening on /menu_index and publishing to /order...")
+    rospy.loginfo("Listening on /menu_index and publishing to /id_order...")
     rospy.spin()
 
 if __name__ == '__main__':
--- src/gui/selected_menu.py
+++ src/gui/selected_menu.py
@@ -1,1 +1,1 @@
-menu_index = [3]
+menu_index = [4, 5, 5, 5, 6, 6, 6, 6, 6]
